c0a34fa8daa4984e2cf91703bc30f252819eb1d6
Rudra Narayan Panda <95025048+rudrapanda257@users.noreply.github.com>
2024-06-22 21:36:28 +0530

    Update Navbar.jsx02

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ function Navbar() {
             <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
               <span className="absolute -inset-1.5"></span>
               <span className="sr-only">Open user menu</span>
-              <img className="h-8 w-8 rounded-full" src="https://drive.google.com/file/d/15O6wX5dYj1ReQwyujsGYs0dJyVsF8ZQm/view" alt=""/>
+              <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
             </button>
           </div>
 
